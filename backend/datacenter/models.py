9b2f58cb42a69f22e8f711b18011cf8838e94fa3
paraigor <icid0096@yandex.ru>
2024-08-22 17:55:12 +0300

    Допилил модели

--- backend/datacenter/models.py
+++ backend/datacenter/models.py
@@ -1,109 +1,115 @@
-from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 import datetime as dt
+
+from django.db import models
+from django.forms import ImageField
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Client(models.Model):
-
     id_tg = models.IntegerField(
         "id в телеграмм", blank=True, null=True, unique=True
     )
     full_name = models.CharField("ФИО", max_length=200)
-    phone_number = PhoneNumberField(null=True, region="RU")
+    phone_number = PhoneNumberField("Номер телефона", null=True, region="RU")
 
     def __str__(self) -> str:
         return self.full_name
 
     class Meta:
-        verbose_name = 'Клиент',
-        verbose_name_plural = 'Клиенты'
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
 
 
 class Level(models.Model):
-
     title = models.CharField("Название уровня", max_length=200)
-    price = models.FloatField('Цена')
+    price = models.FloatField("Цена")
 
     def __str__(self) -> str:
         return self.title
 
     class Meta:
-        verbose_name = 'Размер/уровень для торта',
-        verbose_name_plural = 'Размеры/уровни для торта'
+        verbose_name = "Размер/уровень для торта"
+        verbose_name_plural = "Размеры/уровни для торта"
 
 
 class Form(models.Model):
     title = models.CharField("Название формы", max_length=200)
-    price = models.FloatField('Цена')
+    price = models.FloatField("Цена")
 
     def __str__(self) -> str:
         return self.title
 
     class Meta:
-        verbose_name = 'Форма для торта',
-        verbose_name_plural = 'Формы для торта'
+        verbose_name = "Форма для торта"
+        verbose_name_plural = "Формы для торта"
 
 
 class Topping(models.Model):
     title = models.CharField("Название топпинга", max_length=200)
-    price = models.FloatField('Цена')
+    price = models.FloatField("Цена")
 
     def __str__(self) -> str:
         return self.title
 
     class Meta:
-        verbose_name = 'Топпинг для торта',
-        verbose_name_plural = 'Топпинги для торта'
+        verbose_name = "Топпинг для торта"
+        verbose_name_plural = "Топпинги для торта"
 
 
 class Berry(models.Model):
     title = models.CharField("Название ягоды", max_length=200)
-    price = models.FloatField('Цена')
+    price = models.FloatField("Цена")
 
     def __str__(self) -> str:
         return self.title
 
     class Meta:
-        verbose_name = 'Ягода для торта',
-        verbose_name_plural = 'Ягоды для торта'
+        verbose_name = "Ягода для торта"
+        verbose_name_plural = "Ягоды для торта"
 
 
 class Decor(models.Model):
     title = models.CharField("Название декора", max_length=200)
-    price = models.FloatField('Цена')
+    price = models.FloatField("Цена")
 
     def __str__(self) -> str:
         return self.title
 
     class Meta:
-        verbose_name = 'Декор для торта',
-        verbose_name_plural = 'Декоры для торта'
+        verbose_name = "Декор для торта"
+        verbose_name_plural = "Декоры для торта"
 
 
 class Cake(models.Model):
-    title = models.CharField(
-        "Название торта", max_length=200, null=True, blank=True
-    )
+    title = models.CharField("Название торта", max_length=200)
     price = models.FloatField("Цена торта")
+    image = models.ImageField("Изображение торта", null=True, blank=True)
     level = models.ForeignKey(
-        Level, on_delete=models.PROTECT,
-        verbose_name="Количество уровней торта"
+        Level,
+        on_delete=models.PROTECT,
+        verbose_name="Количество уровней торта",
     )
     form = models.ForeignKey(
-        Form, on_delete=models.PROTECT, verbose_name="Форма"
+        Form, on_delete=models.PROTECT, verbose_name="Форма торта"
     )
     topping = models.ForeignKey(
         Topping, on_delete=models.PROTECT, verbose_name="Топпинг"
     )
     berry = models.ForeignKey(
-        Berry, on_delete=models.PROTECT, verbose_name="Ягода",
-        null=True, blank=True
+        Berry,
+        on_delete=models.PROTECT,
+        verbose_name="Ягода",
+        null=True,
+        blank=True,
     )
     decor = models.ForeignKey(
-        Decor, on_delete=models.PROTECT, verbose_name="Декор",
-        null=True, blank=True
+        Decor,
+        on_delete=models.PROTECT,
+        verbose_name="Декор",
+        null=True,
+        blank=True,
     )
-    inscription = models.CharField(
+    caption = models.CharField(
         "Надпись на торте", max_length=200, null=True, blank=True
     )
 
@@ -111,15 +117,15 @@ class Cake(models.Model):
         return self.title
 
     class Meta:
-        verbose_name = 'Торт',
-        verbose_name_plural = 'Торты'
+        verbose_name = "Торт"
+        verbose_name_plural = "Торты"
 
 
-class Order(models.Model):
+class Invoice(models.Model):
     STATUS = [
         ("waiting", "Ожидает оплаты"),
         ("paid", "Оплачено"),
-        ("cancel", "Отменено"),
+        ("canceled", "Отменено"),
     ]
     client = models.ForeignKey(
         Client, on_delete=models.PROTECT, verbose_name="Клиент"
@@ -133,32 +139,63 @@ class Order(models.Model):
         return f"{self.updated_at} {self.client.full_name} {self.status}"
 
     class Meta:
-        verbose_name = 'Счет на оплату',
-        verbose_name_plural = 'Счета на оплату'
+        verbose_name = "Счет на оплату"
+        verbose_name_plural = "Счета на оплату"
 
 
-class Appointment(models.Model):
+class Address(models.Model):
+    city = models.CharField("Город", max_length=200)
+    street = models.CharField("Улица, номер дома", max_length=200)
+    flat = models.IntegerField("Номер квартиры", null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.city}, {self.street}-{self.flat}"
+
+    class Meta:
+        verbose_name = "Адрес"
+        verbose_name_plural = "Адреса"
+
+
+class Order(models.Model):
     STATUSES = [
-        ("accepted", "Принято"),
-        ("ended", "Завершено"),
-        ("discard", "Отменено"),
+        ("accepted", "Принят"),
+        ("closed", "Закрыт"),
+        ("canceled", "Отменен"),
     ]
     status = models.CharField("Статус заказа", max_length=9, choices=STATUSES)
-    date = models.DateField("Дата доставки")
-    time = models.TimeField("Время доставки")
-    extra_delivery_speed = models.BooleanField(
-        "Доп. плата за скорость доставки", default=False
+    date = models.DateField("Дата заказа", auto_now_add=True)
+    time = models.TimeField("Время заказа", auto_now_add=True)
+    client = models.ForeignKey(
+        Client, on_delete=models.PROTECT, verbose_name="Клиент"
     )
-    client = models.ForeignKey(Client, on_delete=models.PROTECT)
-    cake = models.ForeignKey(Cake, on_delete=models.PROTECT)
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    cake = models.ForeignKey(
+        Cake, on_delete=models.PROTECT, verbose_name="Торт"
+    )
+    delivery_date = models.DateField("Дата доставки")
+    delivery_time = models.TimeField("Время доставки")
+    delivery_address = models.ForeignKey(
+        Address,
+        on_delete=models.PROTECT,
+        verbose_name="Адрес доставки",
+        null=True,
+        blank=True,
+    )
+    invoice = models.ForeignKey(
+        Invoice, on_delete=models.PROTECT, verbose_name="Счет на оплату"
+    )
+    comment = models.CharField(
+        "Комментарий для курьера",
+        max_length=200,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self) -> str:
         return f"{self.status} {self.date} {self.cake.title} {self.client.full_name}"
 
     class Meta:
-        verbose_name = 'Заказ',
-        verbose_name_plural = 'Заказы'
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
 
 
 class TimeFrames(models.Model):
@@ -176,5 +213,21 @@ class TimeFrames(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Временные рамки',
-        verbose_name_plural = 'Временные рамки'
+        verbose_name = "Временные рамки"
+        verbose_name_plural = "Временные рамки"
+
+
+class AdvLink(models.Model):
+    url = models.URLField("Ссылка")
+    visits_number = models.IntegerField(
+        "Количество визитов",
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return self.url
+
+    class Meta:
+        verbose_name = "Рекламная ссылка"
+        verbose_name_plural = "Рекламные ссылки"
