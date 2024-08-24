72fdb138c79b81e9ff77eec97749c598c82cc306
e13q <110967581+e13q@users.noreply.github.com>
2024-08-24 03:19:07 +0300

    bot start flake8 and DRY fix

--- backend/telegram_bot/bot_new.py
+++ backend/telegram_bot/bot_new.py
@@ -1,9 +1,7 @@
-import datetime as dt
 import logging
 
 from django.conf import settings
 from telegram import (
-    Bot,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
     ParseMode,
@@ -13,16 +11,11 @@ from telegram.ext import (
     CallbackContext,
     CallbackQueryHandler,
     CommandHandler,
-    Filters,
-    MessageHandler,
     Updater,
 )
 
 
 from .db_querrys import check_client, create_client, get_time_frame
-from .common_handler_functions import (
-    get_phone_number
-)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -43,27 +36,27 @@ MAXIMUM_EXPEDITED_LEAD_TIME = time_frame.maximum_expedited_lead_time
 URL_AGREEMENT = "https://disk.yandex.ru/i/8a4x4M9KB8A3qw"
 
 
+# Согласие на обработку ПД
 def prestart_PD_request(update: Update, context: CallbackContext):
     context.user_data["user_initial"] = update.message.from_user
-    if not check_client(context.user_data["user_initial"]["id"]):
-        keyboard = [
-            [InlineKeyboardButton("Согласен", callback_data="register_user")]
-        ]
-        update.message.reply_text(
-            f"""
-    Добро пожаловать в бота магазина тортов\\!
-    Для продолжения работы с ботом нам потребуется Ваше согласие на обработку персональных данных\\.
-    С документом Вы можете ознакомиться [по ссылке]({URL_AGREEMENT})\\.
-    Нажимая "Согласен" \\- Вы даёте своё согласие и можете продолжать пользоваться ботом\\.
-    """,
-            parse_mode=ParseMode.MARKDOWN_V2,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(keyboard),
-        )
-    else:
-        start(update, context)
+    keyboard = [
+        [InlineKeyboardButton("Согласен", callback_data="reg_user")]
+    ]
+    welcome_message = f"""
+Добро пожаловать в бота магазина тортов\\!
+Для продолжения работы с ботом нам потребуется Ваше согласие на обработку персональных данных\\.
+С документом Вы можете ознакомиться [по ссылке]({URL_AGREEMENT})\\.
+Нажимая "Согласен" \\- Вы даёте своё согласие и можете продолжать пользоваться ботом\\.
+"""  # noqa: E501,
+    update.message.reply_text(
+        welcome_message,
+        parse_mode=ParseMode.MARKDOWN_V2,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(keyboard),
+    )
 
 
+# Регистрация пользователя
 def reg_user(update: Update, context: CallbackContext):
     user = context.user_data["user_initial"]
     if not check_client(user["id"]):
@@ -77,21 +70,23 @@ def reg_user(update: Update, context: CallbackContext):
     start(update, context)
 
 
+# Старт бота
+# Если пользователя нет в бд - согласие ПД, регистрация
 def start(update: Update, context: CallbackContext):
+    context.user_data.clear()
     context.user_data["client_id"] = update.effective_chat.id
     if not check_client(context.user_data["client_id"]):
         prestart_PD_request(update, context)
         return
     keyboard = [
         [InlineKeyboardButton("Наши предложения", callback_data="list_cakes")],
-        [InlineKeyboardButton("Рекомендация торта", callback_data="recomend_cake")],
+        [InlineKeyboardButton("Рекомендация торта", callback_data="recomend_cake")],  # noqa: E501,
         [InlineKeyboardButton("Собрать свой торт", callback_data="make_cake")],
         [InlineKeyboardButton("Заказы", callback_data="orders")]
     ]
     start_message = f"""Здравствуйте!
 Заказы обрабатываются от {MINIMUM_LEDA_TIME} рабочих часов.
 Рабочие часы: с {WORKDAY_START} до {WOKRDAY_END} мск"""
-
     query = update.callback_query
     if query:
         query.answer()
@@ -113,16 +108,7 @@ def main():
         CallbackQueryHandler(start, pattern="^start$")
     )
     updater.dispatcher.add_handler(
-        CallbackQueryHandler(reg_user, pattern="^register_user$")
+        CallbackQueryHandler(reg_user, pattern="^reg_user$")
     )
-    # updater.dispatcher.add_handler(
-    #     CallbackQueryHandler(cake_handler, pattern="^cake_handle")
-    # )
-    # updater.dispatcher.add_handler(
-    #     CallbackQueryHandler(date_handler, pattern="^date_handle")
-    # )
-    # updater.dispatcher.add_handler(
-    #     CallbackQueryHandler(time_handler, pattern="^time_handle")
-    # )
     updater.start_polling()
     updater.idle()
